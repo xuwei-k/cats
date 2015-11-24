5b426cc249bd1f46d92b8d2af5d514efe5db8915
xuwei-k <6b656e6a69@gmail.com>
2015-11-25 06:39:56 +0900

    ambiguous implicits

--- tests/src/test/scala/cats/tests/IdTTests.scala
+++ tests/src/test/scala/cats/tests/IdTTests.scala
@@ -1,11 +1,13 @@
 package cats.tests
 
-import cats.{Monad, Traverse}
+import cats.{Monad, Traverse, Functor}
 import cats.data.IdT
 import cats.laws.discipline.{MonadTests, SerializableTests, TraverseTests}
 import cats.laws.discipline.arbitrary._
 
 class IdTTests extends CatsSuite {
+
+  def idTFunctor[F[_]: Monad] = implicitly[Functor[IdT[F, ?]]]
 
   checkAll("IdT[List, Int]", MonadTests[IdT[List, ?]].monad[Int, Int, Int])
   checkAll("Monad[IdT[List, ?]]", SerializableTests.serializable(Monad[IdT[List, ?]]))
